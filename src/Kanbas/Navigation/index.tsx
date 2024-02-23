3a99ad70dc57b0fbae3eeefbbf7ecbe3d2107b71
moreheadl <morehead.l@northeastern.edu>
2024-02-22 23:58:42 -0500

    up to 3.5

---
+++ src/Kanbas/Navigation/index.tsx
@@ -0,0 +1,44 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+import { FaTachometerAlt, FaRegUserCircle, FaBook, FaRegCalendarAlt, FaInbox, FaRegClock, FaRegQuestionCircle } 
+    from "react-icons/fa";
+import NEU from "./neu";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { LuMonitorPlay } from "react-icons/lu";
+
+
+
+function KanbasNavigation() {
+  const links = [
+    { label: "Account",   icon: <FaRegUserCircle className="fs-2" />  },
+    { label: "Dashboard", icon: <FaTachometerAlt className="fs-2 wd-kanbas-navigation-i-color" />  },
+    { label: "Courses",   icon: <FaBook className="fs-2 wd-kanbas-navigation-i-color" />           },
+    { label: "Calendar",  icon: <FaRegCalendarAlt className="fs-2 wd-kanbas-navigation-i-color" /> },
+  ];
+  const not_linked = [
+    { label: "Inbox",   icon: <FaInbox className="fs-2 wd-kanbas-navigation-i-color"/>},
+    { label: "History", icon: <FaRegClock className="fs-2 wd-kanbas-navigation-i-color" />  },
+    { label: "Studio",   icon: <LuMonitorPlay className="fs-2 wd-kanbas-navigation-i-color" />           },
+    { label: "Commons",  icon: <FaArrowRightFromBracket className="fs-2 wd-kanbas-navigation-i-color" /> },
+    { label: "Help",  icon: <FaRegQuestionCircle className="fs-2 wd-kanbas-navigation-icon-size wd-kanbas-navigation-i-color" /> },
+  ];
+  
+  const { pathname } = useLocation();
+  return (
+    <ul className="wd-kanbas-navigation">
+        <li><NEU/></li>
+      {links.map((link, index) => (
+        <li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
+          <Link to={`/Kanbas/${link.label}`}> {link.icon} {link.label} </Link>
+        </li>
+      ))}
+      {not_linked.map((navItem, val) => (
+        <li key={val} className={pathname.includes(navItem.label) ? "wd-active" : ""}>
+            <Link to={`#`}> {navItem.icon} {navItem.label} </Link>
+        </li>
+      ))};
+      
+    </ul>
+  );
+}
+export default KanbasNavigation;
---
+++ src/Kanbas/Navigation/neu.tsx
@@ -0,0 +1,7 @@
+function NEU() {
+    return (
+        <img src="/images/northeastern_logo.jpg" className="wd-kanbas-navigation-logo-width" />
+    );
+}
+
+export default NEU;
